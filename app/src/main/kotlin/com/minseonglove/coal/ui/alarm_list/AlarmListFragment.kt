7bd2d1f5f60d2ec574fdd7240cb69d9e5c59e765
minseonglove <qnfpdlr124@naver.com>
2022-04-24 23:45:43 +0900

    FEAT : 조건 설정 레이아웃 추가
    * 감시할 지표와 세부조건을 설정하는 레이아웃 입니다.
    * AlarmList와 CoinSearch 프래그먼트에 include 됩니다.

--- app/src/main/kotlin/com/minseonglove/coal/ui/alarm_list/AlarmListFragment.kt
+++ app/src/main/kotlin/com/minseonglove/coal/ui/alarm_list/AlarmListFragment.kt
@@ -1,14 +1,13 @@
 package com.minseonglove.coal.ui.alarm_list
 
 import android.os.Bundle
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.minseonglove.coal.R
 import com.minseonglove.coal.databinding.FragmentAlarmListBinding
 import com.orhanobut.logger.AndroidLogAdapter
@@ -43,8 +42,13 @@ class AlarmListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // 알람 리스트 -> 조건에 맞는 코인 검색
         binding.toolbarAlarmlist.buttonToolbarNavigation.setOnClickListener {
-            Toast.makeText(requireContext(), "asdf", Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_alarmListFragment_to_coinSearchFragment)
+        }
+        // 알람 리스트 -> 알람 설정
+        binding.bottomButtonAlarmlist.buttonBottomAction.setOnClickListener {
+            findNavController().navigate(R.id.action_alarmListFragment_to_alarmSettingFragment)
         }
     }
 }
--- app/src/main/kotlin/com/minseonglove/coal/ui/coin_search/CoinSearchFragment.kt
+++ app/src/main/kotlin/com/minseonglove/coal/ui/coin_search/CoinSearchFragment.kt
@@ -1,6 +1,40 @@
 package com.minseonglove.coal.ui.coin_search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.minseonglove.coal.R
+import com.minseonglove.coal.databinding.FragmentCoinSearchBinding
 
 class CoinSearchFragment : Fragment() {
+
+    private lateinit var _binding: FragmentCoinSearchBinding
+
+    private val binding get() = _binding
+    private val viewModel: CoinSearchViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate<FragmentCoinSearchBinding?>(
+            inflater,
+            R.layout.fragment_coin_search,
+            container,
+            false
+        ).apply {
+            vm = viewModel
+            lifecycleOwner = this@CoinSearchFragment
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
