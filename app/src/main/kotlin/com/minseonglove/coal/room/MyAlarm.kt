a6e3648df2c4efcfbaec2c8fa4a4c11480febd91
minseonglove <qnfpdlr124@naver.com>
2022-04-25 00:48:50 +0900

    FEAT : roomDB 테이블과 query 추가
    * 사용자가 설정한 알람을 담는 db 입니다.
    * AlarmSetting 프래그먼트에서 입력한 내용을 저장합니다.

---
+++ app/src/main/kotlin/com/minseonglove/coal/room/MyAlarm.kt
@@ -0,0 +1,25 @@
+package com.minseonglove.coal.room
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class MyAlarm(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    @ColumnInfo(name = "coin_name") val coinName: String,
+    @ColumnInfo(name = "minute") val minute: Int,
+    @ColumnInfo(name = "indicator") val indicator: Int,
+    @ColumnInfo(name = "price") val price: Double?,
+    @ColumnInfo(name = "price_condition") val priceCondition: Int?,
+    @ColumnInfo(name = "candle") val candle: Int?,
+    @ColumnInfo(name = "moving_average_condition") val movingAverageCondition: Int?,
+    @ColumnInfo(name = "stochastic_k") val stochasticK: Int?,
+    @ColumnInfo(name = "stochastic_m") val stochasticM: Int?,
+    @ColumnInfo(name = "stochastic_cross") val stochasticCross: Int?,
+    @ColumnInfo(name = "macd_m") val macdM: Int?,
+    @ColumnInfo(name = "value") val value: Double?,
+    @ColumnInfo(name = "value_condition") val valueCondition: Int?,
+    @ColumnInfo(name = "signal") val signal: Int?,
+    @ColumnInfo(name = "signal_condition") val signalCondition: Int?
+)
---
+++ app/src/main/kotlin/com/minseonglove/coal/room/MyAlarmDao.kt
@@ -0,0 +1,17 @@
+package com.minseonglove.coal.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface MyAlarmDao {
+    @Query("SELECT * FROM MyAlarm")
+    fun getAll(): List<MyAlarm>
+
+    @Insert
+    fun insert(myAlarm: MyAlarm)
+
+    @Query("DELETE FROM MyAlarm WHERE id = :id")
+    fun deleteById(id: Int)
+}
