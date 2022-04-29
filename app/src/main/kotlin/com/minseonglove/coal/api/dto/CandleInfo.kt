b2c1540e10bd2ba9cdb3dae20405ae288d31f9da
minseonglove <qnfpdlr124@naver.com>
2022-04-30 01:16:45 +0900

    FIX : 캔들 정보 DTO 변경
    * opening_price(시작가) 제거
    * trade_price(현재가) 추가
    * candle_date_time_kst(현재시간) 추가

--- app/src/main/kotlin/com/minseonglove/coal/api/dto/CandleInfo.kt
+++ app/src/main/kotlin/com/minseonglove/coal/api/dto/CandleInfo.kt
@@ -5,8 +5,10 @@ import com.google.gson.annotations.SerializedName
 data class CandleInfo(
     @SerializedName("market")
     val market: String,
-    @SerializedName("opening_price")
-    val openingPrice: Double,
+    @SerializedName("candle_date_time_kst")
+    val dateTime: String,
+    @SerializedName("trade_price")
+    val tradePrice: Double,
     @SerializedName("high_price")
     val highPrice: Double,
     @SerializedName("low_price")
